9a31a5a52c6c4fba1860b6afbf317ff97a84280e
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-01-18 15:30:23 -0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -10,3 +10,12 @@ VALUES('Agumon', DATE '2020-02-03', '0', 'true', '10.23');
 VALUES('Gabumon', DATE '2018-11-15', '2', 'true', '8.00');
 VALUES('Pikachu', DATE '2021-01-07', '1', 'false', '15.00');
 VALUES('Devimon', DATE '2017-05-12', '5', 'true', '11.00');
+
+/-------------/
+INSERT INTO animals(name, date_of_birth, escape_attempts,neutered,weight_kg) 
+VALUES('Charmander', DATE '2020-02-08','0','false','-11.00');
+VALUES('Plantmon', DATE '2022-11-15','2','true','-5.7');
+VALUES('Squirtle', DATE '1993-04-02','3','false','-12.13');
+VALUES('Angemon', DATE '2005-06-12','1','true','-45.00');
+VALUES('Boarmon', DATE '2005-06-07','7','true','20.4');
+VALUES('Blossom', DATE '1998-11-13','3','true','17.00');
