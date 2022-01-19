dd8727e3a487e11d70890cdec5aa3fc89feef3ac
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-01-18 21:04:25 -0300

    ADD QUERIES

--- queries.sql
+++ queries.sql
@@ -29,6 +29,26 @@ ROLLBACK TRANSACTION;
 SELECT * FROM animals;
 COMMIT TRANSACTION; 
 
+BEGIN TRANSACTION;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SELECT * FROM animals;
+SAVEPOINT firstsv;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SAVEPOINT firstsv;
+SELECT * FROM animals;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+SELECT * FROM animals;
+COMMIT TRANSACTION;
+/------------/
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = '0';
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, AVG(escape_attempts) FROM animals GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, avg(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
+
+ 
+
 
 
 
